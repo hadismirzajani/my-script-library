395bb8fb17c2f4c02839cc5401b3f46601868c58
hadismirzajani <hadismirzajani@gmail.com>
2026-07-09 14:14:52 +1000

    Update Copy-FolderStructure.ps1

--- PowerShell/Copy-FolderStructure.ps1
+++ PowerShell/Copy-FolderStructure.ps1
@@ -10,3 +10,15 @@ Get-ChildItem -Path $Source -Directory -Recurse | ForEach-Object {
     # Create the folder if it doesn't already exist
     New-Item -Path $NewPath -ItemType Directory -Force
 }
+
+or
+
+# If you have thousands of nested folders, standard PowerShell loops can sometimes run slowly. 
+# You can invoke Windows' native ⁠Robocopy Engine directly inside PowerShell to clone the structure in a split second:
+robocopy "C:\SourceFolder" "D:\NewDestination" /e /xf *
+# /e: Copies all subdirectories, including empty ones.
+# /xf *: Excludes all files, ensuring only empty folders are created.
+
+or
+
+Get-ChildItem -Path "C:\SourceFolder" -Directory -Recurse | ForEach-Object { New-Item -Path $_.FullName.Replace("C:\SourceFolder", "D:\NewDestination") -ItemType Directory -Force }
